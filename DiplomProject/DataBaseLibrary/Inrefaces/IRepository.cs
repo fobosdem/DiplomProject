e8a3d79c9e47368fb56138ad36c3d3f2229777b6
Kurilo Dmitriy <fobosdem@gmail.com>
2020-01-09 20:48:55 +0200

    Adding repositoryContext

--- DiplomProject/DataBaseLibrary/Inrefaces/IRepository.cs
+++ DiplomProject/DataBaseLibrary/Inrefaces/IRepository.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataBaseLibrary.Entities
 {
-	public interface IRepository<T, U> where T: class where U: class
+	public interface IRepository<T> where T : class
 	{
 		bool Create(T item);
-		IList<T> GetAll();
-		IList<T> GetAllWith()
+		IList<T> GetAll(bool dependencies = false);
+		bool FindById(int id);
+		bool FindByName(string name);
+		bool Update(T item);
+		bool Delete(T item);
+		bool DeleteById(int id);
 	}
 }
---
+++ DiplomProject/DataBaseLibrary/Repository/EntitiesContext.cs
@@ -0,0 +1,49 @@
+﻿using DataBaseLibrary.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseLibrary.Repository
+{
+	public class EntitiesContext<T> : DbContext where T : class
+	{
+
+		public bool Create(T item)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Delete(T item)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool DeleteById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool FindById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool FindByName(string name)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IList<T> GetAll(bool dependencies = false)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Update(T item)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
