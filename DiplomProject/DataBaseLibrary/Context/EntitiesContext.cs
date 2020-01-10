9adecd692f928046813313f48b5d7f54dcd9baf4
Kurilo Dmitriy <fobosdem@gmail.com>
2020-01-10 15:49:14 +0200

    Adding generic repository for EF

---
+++ DiplomProject/DataBaseLibrary/Context/EntitiesContext.cs
@@ -0,0 +1,16 @@
+﻿using DataBaseLibrary.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseLibrary.Context
+{
+	public class EntitiesContext : DbContext
+	{
+		public DbSet<Chat> Chats { get; set; }
+		public DbSet<User> Users { get; set; }
+	}
+}
