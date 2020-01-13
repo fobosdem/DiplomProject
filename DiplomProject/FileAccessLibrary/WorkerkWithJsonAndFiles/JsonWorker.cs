d147a8576c390ba24d9d971ba7c2df21581c6bcb
Kurilo Dmitriy <fobosdem@gmail.com>
2020-01-13 19:17:34 +0200

    small changes

--- DiplomProject/FileAccessLibrary/WorkerkWithJsonAndFiles/JsonWorker.cs
+++ DiplomProject/FileAccessLibrary/WorkerkWithJsonAndFiles/JsonWorker.cs
@@ -1,12 +1,5 @@
 ﻿using FileAccessLibrary.Entities;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FileAccessLibrary.WorkerkWithJsonAndFiles
 {
